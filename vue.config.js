ff3d9b549032c3ded15f7a206284fbe5fa33e964
神代綺凜 <i@loli.best>
2020-05-03 17:56:57 +0800

    [chore] update dependencies

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ module.exports = {
       skipWaiting: true,
       runtimeCaching: [
         {
-          urlPattern: new RegExp('^https://cdn\\.bootcss\\.com/'),
+          urlPattern: /^https:\/\/cdn\.jsdelivr\.net\//,
           handler: 'CacheFirst',
           options: {
             cacheableResponse: {
@@ -23,14 +23,38 @@ module.exports = {
         },
       ],
     },
-    name: '黑白生草图制作器',
+    name: '黑白图制作器',
     themeColor: '#212121',
     backgroundColor: '#212121',
     msTileColor: '#212121',
     appleMobileWebAppStatusBarStyle: 'black',
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-180x180.png',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png',
+      maskIcon: 'img/icons/mask-icon-16x16.svg',
+    },
     manifestOptions: {
+      name: '黑白图制作器',
+      short_name: '黑白图制作',
       lang: 'zh-Hans',
       background_color: '#212121',
+      description: '快速制作，一键生草',
+      icons: [
+        {
+          src: './assets/icons/default-icon-192x192-maskable.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+        {
+          src: './assets/icons/default-icon-512x512-maskable.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+      ],
     },
   },
 };
